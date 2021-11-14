a7f422ddc698abb437b1c1f3a08ff9b50b1d6e74
nehemiahjoel100 <94307872+nehemiahjoel100@users.noreply.github.com>
2021-11-14 23:52:22 +0300

    c file
    anyway

---
+++ wit.c
@@ -0,0 +1,56 @@
+#include<stdio.h>
+
+int main(){
+
+char alphabet;
+
+char vowel;
+printf("enter alphabet");
+
+scanf("%d",&alphabet);
+
+switch(vowel){
+case'a':
+printf("this is a vowel");
+break;
+
+case'e':
+printf("this is a vowel");
+break;
+
+case'i':
+printf("this is a vowel");
+break;
+
+
+case'o':
+printf("this is a vowel");
+break;
+case'u':
+printf("this is a vowel");
+break;
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+return 0;
+
+}
